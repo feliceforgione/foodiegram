67fb5c16935b4224bba9cb18b9dc05063f576307
Felice Forgione <forgione67@gmail.com>
2023-10-06 22:50:02 -0400

    Added food detection

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -1,11 +1,10 @@
-import { ArrowBackIos, PinDrop } from "@mui/icons-material";
+import { ArrowBackIos, Close } from "@mui/icons-material";
 import {
   AppBar,
   Avatar,
   Button,
   Dialog,
   Divider,
-  InputAdornment,
   Paper,
   Toolbar,
   Typography,
@@ -22,6 +21,16 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Autocomplete from "react-google-autocomplete";
 
+const ComputerVisionClient =
+  require("@azure/cognitiveservices-computervision").ComputerVisionClient;
+const ApiKeyCredentials = require("@azure/ms-rest-js").ApiKeyCredentials;
+const key = process.env.REACT_APP_MS_COMPUTER_VISION_SUBSCRIPTION_KEY;
+const endpoint = process.env.REACT_APP_MS_COMPUTER_VISION_ENDPOINT;
+const computerVisionClient = new ComputerVisionClient(
+  new ApiKeyCredentials({ inHeader: { "Ocp-Apim-Subscription-Key": key } }),
+  endpoint
+);
+
 const initialValue = [
   {
     type: "paragraph",
@@ -34,6 +43,8 @@ const AddPostDialog = ({ media, handleClose }) => {
   const [editor] = React.useState(() => withReact(createEditor()));
   const [value, setValue] = React.useState(initialValue);
   const [location, setLocation] = React.useState("");
+  const [imageTags, setImageTags] = React.useState(null);
+  const [imageIsFoodItem, setImageIsFoodItem] = React.useState(true);
   const classes = useAddPostDialogStyles();
   const [submitting, setSubmitting] = React.useState(false);
   const [createPost] = useMutation(CREATE_POST);
@@ -82,78 +93,113 @@ const AddPostDialog = ({ media, handleClose }) => {
     handleClose();
   }
 
+  function formatTags(tags) {
+    return tags.map((tag) => `${tag.name}`).join(", ");
+  }
+
+  React.useEffect(() => {
+    async function detectFoodInImage() {
+      async function getImageTags(imageURL) {
+        const tags = (
+          await computerVisionClient.analyzeImage(imageURL, {
+            visualFeatures: ["Tags"],
+          })
+        ).tags;
+
+        return tags;
+      }
+      const tags = await getImageTags(media.url);
+      setImageTags(tags);
+      setImageIsFoodItem(
+        tags.findIndex((tag) => tag.name === "food") === -1 ? false : true
+      );
+    }
+    detectFoodInImage();
+  }, [media.url]);
+
   return (
     <Dialog fullScreen open onClose={handleClose}>
-      <AppBar sx={classes.appBar}>
-        <Toolbar sx={classes.toolbar}>
-          <ArrowBackIos onClick={discardPost} />
-          <Typography align="center" variant="body1" sx={classes.title}>
-            New Post
-          </Typography>
-          <Button
-            color="primary"
-            sx={classes.share}
-            disabled={submitting}
-            onClick={handleSharePost}
-          >
-            Share
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Divider />
-      <Paper sx={classes.paper}>
-        <Avatar src={me.profile_image} />
-        <Slate
-          editor={editor}
-          value={initialValue}
-          onChange={(value) => setValue(value)}
-        >
-          <Editable
-            style={{ ...classes.editor }}
-            placeholder="write your caption..."
+      {imageIsFoodItem ? (
+        <>
+          <AppBar sx={classes.appBar}>
+            <Toolbar sx={classes.toolbar}>
+              <ArrowBackIos onClick={discardPost} />
+              <Typography align="center" variant="body1" sx={classes.title}>
+                New Post
+              </Typography>
+              <Button
+                color="primary"
+                sx={classes.share}
+                disabled={submitting}
+                onClick={handleSharePost}
+              >
+                Share
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Divider />
+          <Paper sx={classes.paper}>
+            <Avatar src={me.profile_image} />
+            <Slate
+              editor={editor}
+              value={initialValue}
+              onChange={(value) => setValue(value)}
+            >
+              <Editable
+                style={{ ...classes.editor }}
+                placeholder="write your caption..."
+              />
+            </Slate>
+            <Avatar src={media.url} sx={classes.avatarLarge} variant="square" />
+          </Paper>
+          {imageTags && (
+            <p
+              style={{
+                padding: "0 10px 0 10px",
+              }}
+            >
+              <strong>Potential Image Tags: </strong>
+              {formatTags(imageTags)}
+            </p>
+          )}
+          <Autocomplete
+            apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+            onPlaceSelected={(place) => {
+              establishmentRef.current = place;
+            }}
+            style={{
+              width: "100%",
+              height: "40px",
+              fontSize: "20px",
+              marginTop: "20px",
+            }}
+            placeholder="Enter Establishment Name and Location"
+            options={{
+              componentRestrictions: { country: "us" },
+              fields: ["geometry", "name", "url", "formatted_address"],
+              strictBounds: false,
+              types: ["establishment"],
+            }}
           />
-        </Slate>
-        <Avatar src={media.url} sx={classes.avatarLarge} variant="square" />
-      </Paper>
-      {/*       <TextField
-        fullWidth
-        placeholder="Location"
-        sx={{ ...classes.root, ...classes.underline }}
-        InputProps={{
-          sx: {
-            ".MuiInputBase-input": { ...classes.input },
-          },
-
-          startAdornment: (
-            <InputAdornment position="start">
-              <PinDrop />
-            </InputAdornment>
-          ),
-        }}
-        onChange={(e) => setLocation(e.target.value)}
-        value={location}
-      /> */}
-
-      <Autocomplete
-        apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-        onPlaceSelected={(place) => {
-          console.log(place);
-          establishmentRef.current = place;
-        }}
-        style={{
-          width: "100%",
-          height: "40px",
-          fontSize: "20px",
-          marginTop: "20px",
-        }}
-        placeholder="Enter Establishment Name and Location"
-        options={{
-          componentRestrictions: { country: "us" },
-          fields: ["geometry", "name", "url", "formatted_address"],
-          strictBounds: false,
-          types: ["establishment"],
-        }}
-      />
+        </>
+      ) : (
+        <>
+          <AppBar sx={classes.appBar}>
+            <Toolbar sx={classes.toolbar}>
+              <ArrowBackIos onClick={discardPost} />
+              <Typography align="center" variant="body1" sx={classes.title}>
+                Image is not a Food Item
+              </Typography>
+              <Close onClick={discardPost} />
+            </Toolbar>
+          </AppBar>
+          <Divider />
+          <p></p>
+          <Typography align="center" variant="body1">
+            Please upload another picture that is a food item.
+          </Typography>
+        </>
+      )}
     </Dialog>
   );
 };
